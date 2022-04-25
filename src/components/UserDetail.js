3d80c74596c8fa2dc8c7c4f8cc97bd3142551d97
Marcos8060 <marcosgav80@gmail.com>
2022-04-25 11:05:14 +0300

    created a usestate data

---
+++ src/components/UserDetail.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+
+function UserDetail() {
+    var [userDetails, setUserDetails] = useState({
+        name: "Mayank",
+        age: 30
+      });
+      
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default UserDetail
